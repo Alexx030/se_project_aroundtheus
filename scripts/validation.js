07271ad3b441d2bb7cebc3c6e8dac1bced75bc39
Alexx Banchongchith <alexxbanchon@gmail.com>
2023-10-02 22:12:32 -0600

    Update Validation.JS, Modal.CSS

--- scripts/validation.js
+++ scripts/validation.js
@@ -1,7 +1,24 @@
+function showInputError(formEl, inputEl, options) {
+    const errorMessageEl = formEl.querySelector(`#${inputEl.id}-error`);
+    console.log(errorMessageEl);
+}
+
+function checkInputValidity(formEl, inputEl, options) {
+    if(!inputEl.validity.valid) {
+        showInputError(formEl, inputEl, options);
+    } else {
+        hideInputError(formEl, inputEl, options);
+    }
+}
+
 function setEventListeners(formEl, options) {
   const { inputSelector } = options;
   const inputEls = [...formEl.querySelectorAll(inputSelector)];
-  console.log(inputEls);
+  inputEls.forEach(inputEl) => {
+    inputEl.addEventListener("input", (evt) => {
+        checkInputValidity(formEl, inputEl, options);
+    });
+  }
 }
 
 function enableValidation(options) {
@@ -15,7 +32,7 @@ function enableValidation(options) {
   });
 }
 
-const config = {
+const config = { 
   formSelector: ".modal__form",
   inputSelector: ".modal__input",
   submitButtonSelector: ".modal__button",
